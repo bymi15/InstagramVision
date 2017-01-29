cc19c89be50b22632ca3c359a917a429b2b738c7
Brian Min <bymi15@yahoo.com>
2017-01-29 11:00:22 +0000

    updated path for heroku app

--- path.php
+++ path.php
@@ -4,6 +4,6 @@ if ( !defined('ABSPATH') )
     define('ABSPATH', $_SERVER["DOCUMENT_ROOT"] . '/');
 
 if ( !defined('ROOTPATH') )
-    define('ROOTPATH', 'http://www.instavision.hol.es/');
+    define('ROOTPATH', 'https://instagramvision.herokuapp.com/');
 
 ?>
